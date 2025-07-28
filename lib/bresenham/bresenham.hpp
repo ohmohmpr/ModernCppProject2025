71d9086077ab1ca6d9edcb3defa6690dfadee1e6
Panyawat Rattana <nanoic_ohm@hotmail.com>
2025-07-28 14:32:50 +0200

    [ADD] init bresenham

--- lib/bresenham/bresenham.hpp
+++ lib/bresenham/bresenham.hpp
@@ -9,14 +9,14 @@ using Vector3dVector = std::vector<Eigen::Vector3d>;
 using PoseAndCloud = std::pair<Eigen::Matrix4d, Vector3dVector>;
 
 namespace bresenham {
-class Dataset {
+class Bresenham {
 public:
-  Dataset(const std::string &data_dir);
-  std::size_t size() const { return pointcloud_files_.size(); }
-  PoseAndCloud operator[](const int idx) const;
+  Bresenham(Eigen::Vector3i &pose_on_map, Eigen::Vector3i &cloud_on_map);
+  // std::size_t size() const { return pointcloud_files_.size(); }
+  // PoseAndCloud operator[](const int idx) const;
 
 private:
-  std::vector<std::string> pointcloud_files_;
-  std::vector<Eigen::Matrix4d> poses_;
+  // std::vector<std::string> pointcloud_files_;
+  // std::vector<Eigen::Matrix4d> poses_;
 };
 } // namespace bresenham
